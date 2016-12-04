109a31271903332caa0ae783c0989f7339185302
Vladislav Pilipenko <pilipenko@banki.ru>
2016-12-04 08:25:52 +0300

    First, extremely simple File.createNewFile() test. It just checks that created file does not exist yet. And it creates the temporary folder beforehand to delete it after the test.

--- src/test/java/com/softwaretestingtraning/app/AppTest.java
+++ src/test/java/com/softwaretestingtraning/app/AppTest.java
@@ -1,17 +1,52 @@
 package com.softwaretestingtraning.app;
 
+import org.apache.commons.io.FileUtils;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Test
 public class AppTest {
 
-    @Test()
-    public void testFirst() {
+    private Path tempDirectory;
+    private String fileName;
 
-        App obj = new App();
-
-        Assert.assertNotNull(obj);
-
+    @BeforeMethod
+    public void exampleBeforeMethod() {
+        System.out.println("Creating temporary directory.");
+        try {
+            tempDirectory = Files.createTempDirectory("test-createNewFile-directory");
+            fileName = tempDirectory.toString() + fileName;
+            System.out.println("TMP: " + tempDirectory.toString());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
+    public void testFirst() {
+        File file = new File(fileName);
+        try {
+            Assert.assertTrue(file.createNewFile(), "File already exists");
+        }
+        catch (IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    @AfterMethod
+    public void exampleAfterMethod(){
+        System.out.println("Removing temporary directory.");
+        try {
+            FileUtils.deleteDirectory(new File(tempDirectory.toString()));
+        }
+        catch (IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
 }
