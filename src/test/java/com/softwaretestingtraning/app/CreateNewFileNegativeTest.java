4a76f4873489b3d4b570fca78cfff52bc22372ba
Vladislav Pilipenko <pilipenko@banki.ru>
2016-12-08 09:58:00 +0300

    Fixed negative tests.

--- src/test/java/com/softwaretestingtraning/app/CreateNewFileNegativeTest.java
+++ src/test/java/com/softwaretestingtraning/app/CreateNewFileNegativeTest.java
@@ -9,14 +9,25 @@ import java.io.IOException;
 @Test
 public class CreateNewFileNegativeTest extends CreateNewFileTestBase {
 
-    public void testAttemptToCreateAFileInNonexistentDirectory() {
-        String fileNameInvalidDirectory = tempDirectory.toString() + "nonexistentDir/" + fileName;
+    public void testAttemptToCreateAFileInWithTheIncorrectFilename() {
+        String fileNameInvalidDirectory = tempDirectory.toString() + "//";
         try {
             File file = new File(fileNameInvalidDirectory);
-            Assert.assertTrue(file.createNewFile(), "File was created successfully in the nonexistent folder.");
+            Assert.assertFalse(file.createNewFile(), "You just successfully created the file named '/'. It's not allowed it Windows and in most -nix distributions.");
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
     }
+
+    public void testAttemptToCreateAFileInWithoutWritingPermissions() {
+        String fileNameInvalidDirectory = tempDirectoryWithoutWritingPermissions.toString() + "/filename";
+        try {
+            File file = new File(fileNameInvalidDirectory);
+            Assert.assertFalse(file.createNewFile(), "You just successfully created the file in the directory without writing permissions.");
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
 }
 
--- src/test/java/com/softwaretestingtraning/app/CreateNewFileTestBase.java
+++ src/test/java/com/softwaretestingtraning/app/CreateNewFileTestBase.java
@@ -10,11 +10,17 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Set;
 
 @Test
 public class CreateNewFileTestBase {
 
     protected Path tempDirectory;
+    protected Path tempDirectoryWithoutWritingPermissions;
     protected String fileName;
 
     @BeforeClass
@@ -22,10 +28,19 @@ public class CreateNewFileTestBase {
         System.out.println("Creating temporary directory.");
         try {
             tempDirectory = Files.createTempDirectory("test-createNewFile-directory");
-            fileName = tempDirectory.toString() + fileName;
+            fileName = tempDirectory.toString() + "/" + fileName;
             System.out.println("TMP: " + tempDirectory.toString());
             File file = new File(fileName);
             Assert.assertTrue(file.createNewFile(), "File already exists. Your cleanup method is probably broken.");
+
+            Set<PosixFilePermission> perms =
+                    PosixFilePermissions.fromString("r-xr-xr-x");
+            FileAttribute<Set<PosixFilePermission>> attr =
+                    PosixFilePermissions.asFileAttribute(perms);
+
+            Path nonWritableDir = Paths.get(tempDirectory.toString(), "nonWritableDir");
+            tempDirectoryWithoutWritingPermissions = Files.createDirectory(nonWritableDir, attr);
+
 
         } catch (IOException e) {
             System.err.println(e.getMessage());
