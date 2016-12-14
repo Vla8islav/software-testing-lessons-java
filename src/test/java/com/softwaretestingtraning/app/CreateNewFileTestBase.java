acdce8f01a8de717cf86d2c1ce250aec196fdd3c
Vladislav Pilipenko <pilipenko@banki.ru>
2016-12-14 22:38:54 +0300

    Added alwaysRun flag to the preparation methods.

--- src/test/java/com/softwaretestingtraning/app/CreateNewFileTestBase.java
+++ src/test/java/com/softwaretestingtraning/app/CreateNewFileTestBase.java
@@ -13,7 +13,7 @@ public class CreateNewFileTestBase {
     Path tempDirectory;
     String fileName;
 
-    @BeforeMethod
+    @BeforeMethod(alwaysRun = true)
     public void exampleBeforeMethod() throws IOException {
         System.out.println("Creating temporary directory.");
         tempDirectory = Files.createTempDirectory("test-createNewFile-directory");
@@ -21,7 +21,7 @@ public class CreateNewFileTestBase {
         System.out.println("TMP: " + tempDirectory.toString());
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void exampleAfterMethod() throws IOException {
         System.out.println("Removing temporary directory.");
         FileUtils.deleteDirectory(new File(tempDirectory.toString()));
