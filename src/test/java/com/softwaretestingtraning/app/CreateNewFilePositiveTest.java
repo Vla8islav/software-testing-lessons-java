f33cd9cff8d04dfd9d04941766e466f1c4c50129
Vladislav Pilipenko <pilipenko@banki.ru>
2016-12-24 17:11:17 +0300

    Removed some garbage dependencies.

--- src/test/java/com/softwaretestingtraning/app/CreateNewFilePositiveTest.java
+++ src/test/java/com/softwaretestingtraning/app/CreateNewFilePositiveTest.java
@@ -14,10 +14,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 
